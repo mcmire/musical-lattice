744e6cb652c2169487c15ae46d824b550dfa1aa5
Elliot Winkler <elliot.winkler@gmail.com>
2018-02-08 13:48:44 -0700

    Ensure that react-hot-loader is not used in production
    * Don't wrap our root React component in AppContainer in prod mode
    * Only include react-hot-loader Babel plugin in dev mode

--- client/index.js
+++ client/index.js
@@ -1,25 +1,39 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { AppContainer } from "react-hot-loader";
 import { BrowserRouter } from "react-router-dom";
 import Root from "./components/Root";
 
 const rootElement = document.querySelector("#root");
+let renderRoot;
 
-function renderRoot() {
-  ReactDOM.render(
-    <AppContainer>
+if (process.env.NODE_ENV === "production") {
+  renderRoot = () => {
+    ReactDOM.render(
       <BrowserRouter>
         <Root />
-      </BrowserRouter>
-    </AppContainer>,
-    rootElement
-  );
+      </BrowserRouter>,
+      rootElement
+    );
+  };
+} else {
+  const { AppContainer } = require("react-hot-loader");
+
+  renderRoot = () => {
+    ReactDOM.render(
+      <AppContainer>
+        <BrowserRouter>
+          <Root />
+        </BrowserRouter>
+      </AppContainer>,
+      rootElement
+    );
+  };
 }
 
 renderRoot();
 
-// Webpack Hot Module Replacement API
-if (module.hot) {
-  module.hot.accept("./components/Root", renderRoot);
+if (process.env.NODE_ENV !== "production") {
+  if (module.hot) {
+    module.hot.accept("./components/Root", renderRoot);
+  }
 }
