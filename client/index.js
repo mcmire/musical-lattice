79a43c94c89c27b3a40df3ddc7179bd67b124b8c
Elliot Winkler <elliot.winkler@gmail.com>
2018-02-09 15:47:07 -0700

    Add normalize.css

--- client/index.js
+++ client/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
+import "normalize.css";
 import "material-components-web/dist/material-components-web.min.css";
 import "material-design-icons/iconfont/material-icons.css";
 
