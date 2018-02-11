df33c847b2c3ee1bb6a136d2de6cc0274fcf620a
Elliot Winkler <elliot.winkler@gmail.com>
2018-02-11 16:35:00 -0700

    Remove users page as well as navigation

--- app/components/Root/index.js
+++ app/components/Root/index.js
@@ -1,26 +1,12 @@
 import React from "react";
-import { Link, Route } from "react-router-dom";
+import { Route } from "react-router-dom";
 import Home from "../Home";
-import Users from "../Users";
 
 export default class Root extends React.Component {
   render() {
     return (
       <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/members">Users</Link>
-            </li>
-          </ul>
-        </nav>
-        <div>
-          <Route path="/" exact={true} component={Home} />
-          <Route path="/members" component={Users} />
-        </div>
+        <Route path="/" exact={true} component={Home} />
       </div>
     );
   }
