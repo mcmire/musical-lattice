1a3488e4d3d8a037f8896fbda319cec72fda9128
Elliot Winkler <elliot.winkler@gmail.com>
2018-02-18 14:15:27 -0700

    Add link to source

--- app/components/Header/index.js
+++ app/components/Header/index.js
@@ -8,15 +8,18 @@ class Header extends React.Component {
   render() {
     return (
       <header styleName="root">
-        <h1 styleName="title">
-          The Lattice
-        </h1>
+        <h1 styleName="title">The Lattice</h1>
         <h2 styleName="subtitle">
           a musical playground by&nbsp;
           <a href="http://twitter.com/mcmire" target="_blank">
             @mcmire
           </a>
         </h2>
+        <p styleName="source">
+          <a href="https://github.com/mcmire/musical-lattice" target="_blank">
+            (Source on GitHub)
+          </a>
+        </p>
       </header>
     );
   }
